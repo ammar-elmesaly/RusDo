e3f8b4f790f6b1376247309f567a077119a1ecc4
ammar-elmesaly <amarmazn4@gmail.com>
2025-10-01 23:49:40 +0300

    fix: change directory of database file to home (~/.rusdo-data/tasks.db)

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,13 @@
 use rusqlite::{Connection, Result};
+use std::{env, fs, path::PathBuf};
 
 pub fn init_db() -> Result<Connection> {
-    let conn = Connection::open("./data/tasks.db")?;
+    let mut path = PathBuf::from(env::var("HOME").unwrap());
+    
+    path.push(".rusdo-data");
+    fs::create_dir_all(&path).unwrap();
+
+    path.push("tasks.db");  // push database file name to path
+    let conn = Connection::open(&path)?;
     Ok(conn)
 }
